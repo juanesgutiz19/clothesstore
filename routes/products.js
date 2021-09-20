60829467a50b7a59fc38c1c158a6b707fd427c99
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2021-09-20 15:03:02 -0500

    Primera validación funciona!

--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 
 const { GetMostWantedProducts, createProducts } = require('../controllers/products');
 const { validateFields } = require('../middlewares/validate-fields');
-
+const { check } = require('express-validator');
 const router = Router();
 
-router.get('/mostwanted' ,GetMostWantedProducts );
+router.get('/mostwanted', GetMostWantedProducts );
 
-router.post('/', createProducts );
+router.post('/',
+    [
+        check('name', 'El name es obligatorio').not().isEmpty(),
+        validateFields
+    ], createProducts );
 
 module.exports = router;
