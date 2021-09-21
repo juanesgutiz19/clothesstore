8cc021eb3523c24c29a0b3872fc6e5efcb2811c0
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2021-09-21 02:04:37 -0500

    Se terminan las validaciones, queda pendiente hacer los tests y crear función para subir archivo a cloudinary

--- controllers/products.js
+++ controllers/products.js
@@ -8,40 +8,82 @@ const Product = require('../models/product');
 const Picture = require('../models/picture');
 
 const { setDiscountPriceToProducts } = require('../helpers/products-utils');
+const { fileIsGreaterThan1MB } = require('../helpers/files-utils');
 
 const GetMostWantedProducts = async(req, res = response) => {
 
     const { limit = 10  } = req.query;
-    const sortedProducts = await Product.find({}, 'price discountPercentage picture name numberOfVisits')
-    .populate('picture', 'urlFrontal urlBack')
-    .sort( { numberOfVisits: -1 } ).limit(Number(limit)).exec();
 
-    const productsWithDiscount = setDiscountPriceToProducts(sortedProducts);
+    try {
+        const sortedProducts = await Product.find({}, 'price discountPercentage picture name numberOfVisits')
+        .populate('picture', 'urlFrontal urlBack')
+        .sort( { numberOfVisits: -1 } ).limit(Number(limit)).exec();
 
-    res.json({
-        productsWithDiscount
-    });
+        const productsWithDiscount = setDiscountPriceToProducts(sortedProducts);
+
+        res.status(200).json({
+            productsWithDiscount
+        });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 }
 
 const createProducts = async(req, res = response) => {
 
-    const { tempFilePath: tempFilePathFrontal } = req.files.urlFrontal;
-    const { tempFilePath: tempFilePathBack } = req.files.urlBack;
-    const { secure_url: urlFrontal } = await cloudinary.uploader.upload( tempFilePathFrontal, { quality: 50 } );
-    const { secure_url: urlBack } = await cloudinary.uploader.upload( tempFilePathBack )
+    const { tempFilePath: tempFilePathFrontal, size: sizeFrontal } = req.files.urlFrontal;
+    const { tempFilePath: tempFilePathBack , size: sizeBack} = req.files.urlBack;
 
-    const picture = new Picture({ urlFrontal, urlBack });
+    try {
+        let urlBack;
+        let urlFrontal;
+        if ( fileIsGreaterThan1MB(sizeFrontal) && fileIsGreaterThan1MB(sizeBack)){
+            const { secure_url: frontal } = await cloudinary.uploader.upload( tempFilePathFrontal, { quality: 50 } );
+            const { secure_url: back } = await cloudinary.uploader.upload( tempFilePathBack, { quality: 50 } );
+            urlFrontal = frontal;
+            urlBack = back;
+        } else if (fileIsGreaterThan1MB(sizeFrontal)){
+            const { secure_url: frontal } = await cloudinary.uploader.upload( tempFilePathFrontal, { quality: 50 } );
+            const { secure_url: back } = await cloudinary.uploader.upload( tempFilePathBack );
+            urlFrontal = frontal;
+            urlBack = back;
+        } else if(fileIsGreaterThan1MB(sizeBack)){
+            const { secure_url: back } = await cloudinary.uploader.upload( tempFilePathBack, { quality: 50 } );
+            const { secure_url: frontal } = await cloudinary.uploader.upload( tempFilePathFrontal);
+            urlBack = back;
+            urlFrontal = frontal;
+        }else {
+            const { secure_url: frontal } = await cloudinary.uploader.upload( tempFilePathFrontal );
+            const { secure_url: back } = await cloudinary.uploader.upload( tempFilePathBack );
+            urlFrontal = frontal;
+            urlBack = back;
+        }
 
-    await picture.save();
+        const picture = new Picture({ urlFrontal, urlBack });
 
-    const { name, description, price, discountPercentage, sellingCountry } = req.body;
+        await picture.save();
 
-    const product = new Product( { name, description, price, discountPercentage, sellingCountry, picture: picture._id } );
+        const { name, description, price, discountPercentage, sellingCountry } = req.body;
 
-    await product.save();
-    res.json({
-        product
-    });
+        const product = new Product( { name, description, price, discountPercentage, sellingCountry, picture: picture._id } );
+
+        await product.save();
+
+        res.status(201).json({
+            product
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 }
 
 module.exports = {
