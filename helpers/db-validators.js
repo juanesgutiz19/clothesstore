8261431ffae3fed3442f12c2bf7385c4b6ddb62d
Juan Esteban Gutiérrez <juanesguti19@gmail.com>
2021-09-22 01:00:51 -0500

    Pequeño cambio en db-validators

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -4,7 +4,7 @@ const isCountryValid = async(code = '') => {
 
     const existsCountry = await Country.findOne({ code });
     if ( !existsCountry ) {
-        throw new Error(`El país con código ${ code } no está registrado en la base de datos`);
+        throw new Error(`El país con código ${ code } no está registrado en la base de datos, revise ISO 3166-1`);
     }
 }
 
